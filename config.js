1f971f65d8b04da76c5c133fb1ed583bd911d649
mneeni <144950149+mneeni@users.noreply.github.com>
2023-09-14 17:36:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -54,9 +54,9 @@ global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada 
 
 /*============== NOMOR ==============*/
 global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
-global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nomorown = '254758644770' //Nomor Owner
+global.namebot = 'Proff-MD-v1' //Nama Bot
+global.nameown = 'proff' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
